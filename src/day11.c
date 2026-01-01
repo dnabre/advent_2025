2ab37297201e6cf4987162e1740560c20c55ee6e
dnabre <dnabre@happybunnylabs.com>
2026-01-01 08:39:29 -0700

    day 11, part2
    not sure where things are at this point (beyond not working). but need break -- so snapshot

--- src/day11.c
+++ src/day11.c
@@ -17,41 +17,46 @@ void day11(const char* filename){
     const struct problem_inputs day11_lines = read_by_lines(filename);
 
     printf("Advent of Code, Day 11\n");
-    printf("    ---------------------------------------------\n");
-    printf("\t part 1: ");
-    char* answer_part1 = day11_part1(day11_lines);
+    if (strcmp(filename, "inputs/2025/day_11_test_02.txt") != 0) {
+        printf("    ---------------------------------------------\n");
+        printf("\t part 1: ");
+        char* answer_part1 = day11_part1(day11_lines);
 
-    printf("\t %s\n", answer_part1);
+        printf("\t %s\n", answer_part1);
 
+        if (answer_part1 != NULL && strcmp(answer_part1, DAY11_PART1_ANSWER) != 0) {
+            fprintf(stderr, "Day 11, Part 1, answer is WRONG. Expected: %s, Received: %s\n",
+                    DAY11_PART1_ANSWER, answer_part1);
+        }
+        if (answer_part1) { free(answer_part1); };
+    }
     printf("\t part 2: ");
     char* answer_part2 = day11_part2(day11_lines);
 
     printf("\t %s\n", answer_part2);
     printf("    ---------------------------------------------\n");
 
-    if (answer_part1 != NULL && strcmp(answer_part1, DAY11_PART1_ANSWER) != 0) {
-        fprintf(stderr, "Day 11, Part 1, answer is WRONG. Expected: %s, Received: %s\n",
-                DAY11_PART1_ANSWER, answer_part1);
-    }
+
     if (answer_part2 != NULL && strcmp(answer_part2, DAY11_PART2_ANSWER) != 0) {
         fprintf(stderr, "Day 11, Part 2, answer is WRONG. Expected: %s, Received: %s\n",
                 DAY11_PART2_ANSWER, answer_part2);
     }
 
 
-    if (answer_part1) { free(answer_part1); };
     if (answer_part2) { free(answer_part2); };
 }
 
 #define C_SPAN 26
 bool seen[C_SPAN][C_SPAN][C_SPAN] = {false};
 size_t seen_count = 0;
+#define MAX_NODE 586
+int64_t cache[MAX_NODE][MAX_NODE];
 
-static char int2char(size_t i){
+char int2char(size_t i){
     return (char)((unsigned char)i + 'a');
 }
 
-static bool mark_seen(const char* str){
+bool mark_seen(const char* str){
     if (seen[str[0] - 'a'][str[1] - 'a'][str[2] - 'a']) {
         return true;
     }
@@ -86,7 +91,7 @@ static void unpack_str(const size_t id, char out[4]){
 
 static char unpack_buffer[4];
 
-static char* s_unpack(const size_t id){
+char* s_unpack(const size_t id){
     if (id == 0) {
         printf("ERROR: unpack_str called on id==0\n");
         exit(-1);
@@ -105,7 +110,6 @@ struct vertex {
     size_t n_edges;
     size_t* edges;
 };
-
 
 
 static void free_vertex(struct vertex* v){
@@ -139,16 +143,24 @@ static size_t count_paths1(size_t l_id, size_t r_id){
     return sum;
 }
 
-static size_t count_paths(size_t l_id, size_t r_id, size_t level){
-    if (l_id == r_id) { return 1; }
-    size_t sum = 0;
-    size_t l_idx = id_to_index[l_id];
-    for (size_t j=0; j<level; j++) {
+
+size_t count_paths_l(size_t l_id, size_t r_id, size_t level){
+    for (size_t j = 0; j < level; j++) {
         printf("\t");
     }
-    printf("id=%zu,idx:%zu =>\n", l_id, l_idx);
+    if (l_id == r_id) {
+        size_t idx = id_to_index[l_id];
+        printf("match @ %s(%zu,%zu)\n", graph[idx].name, l_id, idx);
+        return 1;
+    }
+    size_t sum = 0;
+    size_t l_idx = id_to_index[l_id];
+    for (size_t j = 0; j < level; j++) {
+        printf("\t");
+    }
+    printf("%s (id=%zu,idx:%zu) => \n", graph[l_idx].name, l_id, l_idx);
     for (size_t i = 0; i < graph[l_idx].n_edges; i++) {
-        sum += count_paths(graph[l_idx].edges[i], r_id, level+1);
+        sum += count_paths_l(graph[l_idx].edges[i], r_id, level + 1);
     }
     return sum;
 }
@@ -168,11 +180,12 @@ static void print_vertex(const struct vertex* v){
     }
     printf("]\n");
 }
+
 char* day11_part1(const struct problem_inputs p_i){
-    v_count = p_i.count;
-    struct vertex v[v_count ];
+    v_count = p_i.count + 1;
 
-
+    struct vertex v[v_count];
+    memset(&v, 0, sizeof(struct vertex) * v_count);
     for (size_t i = 0; i < p_i.count; i++) {
         const size_t l_len = strlen(p_i.lines[i]);
         struct fstr_vec* pieces = fsplit_on_char(p_i.lines[i], l_len, ' ');
@@ -192,124 +205,130 @@ char* day11_part1(const struct problem_inputs p_i){
             v[i].edges[r - 1] = pack_str(pieces->arr[r]->str);
         }
         free_fstr_vec(pieces);
-
     }
+    v[v_count - 1].id = pack_str("out");
+    v[v_count - 1].n_edges = 0;
+    v[v_count - 1].edges = NULL;
+    id_to_index[v[v_count - 1].id] = v_count - 1;
 
 
     graph = v;
-    size_t path_count= 0;
+    size_t path_count = 0;
     size_t you_id = pack_str("you");
     size_t out_id = pack_str("out");
 
     path_count = count_paths1(you_id, out_id);;
-
-
+    for (size_t v_i = 0; v_i < v_count; v_i++) {
+        free_vertex(&v[v_i]);
+    }
 
     char* answer = malloc(ANSWER_BUFFER_SIZE);
     sprintf(answer, "%zu", path_count);
     return answer;
 }
 
+
+static size_t count_paths2(size_t l_id, size_t r_id){
+    // printf("count_paths2(%zu, %zu) \n", l_id, r_id);
+    if (l_id == r_id) { return 1; }
+    size_t l_idx = id_to_index[l_id];
+    size_t r_idx = id_to_index[r_id];
+    int64_t c = cache[min_st(l_idx, r_idx)][max_st(l_idx, r_idx)];
+
+    if (c != -1) {
+        // printf("\t\t hit \t\t %s->%s\n" , s_unpack(l_id), s_unpack(r_id));
+        return (size_t)c;
+    }
+
+    size_t sum = 0;
+    // printf("%s =>\n", s_unpack(l_id));
+    for (size_t i = 0; i < graph[l_idx].n_edges; i++) {
+        sum += count_paths2(graph[l_idx].edges[i], r_id);
+    }
+
+    cache[min_st(l_idx, r_idx)][max_st(l_idx, r_idx)] = (int64_t)sum;
+    return sum;
+}
+
+
+
+
+size_t count_paths(char* start, char* end){
+    size_t l_id = pack_str(start);
+    size_t r_id = pack_str(end);
+    printf("\t%s -> %s\t ", start, end);
+    size_t r = count_paths2(l_id, r_id);
+    printf("%zu\n", r);
+    return r;
+}
+
+
+void count_paths_b(char* start, char* end, char* req1, char* req2){
+}
+
 char* day11_part2(const struct problem_inputs p_i){
     printf("\npart2\n");
-    return NULL;
-  v_count = p_i.count;
+    v_count = p_i.count;
     struct vertex v[v_count + 1];
-    bool out_has_vertex = false;
-
+    memset(&v, 0, sizeof(struct vertex) * v_count);
     for (size_t i = 0; i < p_i.count; i++) {
+        // printf("parsing line %zu: %s\n", i, p_i.lines[i]);
         const size_t l_len = strlen(p_i.lines[i]);
         struct fstr_vec* pieces = fsplit_on_char(p_i.lines[i], l_len, ' ');
-        // printf("%3zu: parsed line length: %zu, contents: |%s|\n",i, pieces->len, p_i.lines[i]);
-        // printf("\t%zu parts: \n", pieces->len);
 
         char parse_buffer[4];
         for (size_t j = 0; j < 3; j++) {
             parse_buffer[j] = pieces->arr[0]->str[j];
         }
         parse_buffer[3] = '\0';
-        if (strcmp(parse_buffer, "out") == 0) {
-            out_has_vertex = true;
-        }
-
-        v[i].name[0] = parse_buffer[0];
-        v[i].name[1] = parse_buffer[1];
-        v[i].name[2] = parse_buffer[2];
-        v[i].name[3] = parse_buffer[3];
-
+        strcpy(v[i].name, parse_buffer);
         v[i].id = pack_str(parse_buffer);
         id_to_index[v[i].id] = i;
-        // printf("\tfirst: |%s|, id=%zu, idx=%zu, id_to_index[%zu]=%zu]\n", parse_buffer,v[i].id, i, v[i].id, id_to_index[v[i].id]);
-
 
         const size_t rest_len = pieces->len - 1;
         v[i].n_edges = rest_len;
         v[i].edges = malloc(rest_len * sizeof(size_t));
-        // printf("\n\trest of string has %zu pieces\n", rest_len);
         for (size_t r = 1; r < pieces->len; r++) {
-            // printf("\t\t%zu: %-8s\t packed: %zu\n", r, pieces->arr[r]->str, pack_str(pieces->arr[r]->str));
             v[i].edges[r - 1] = pack_str(pieces->arr[r]->str);
         }
-        // print_vertex(&v[i]);
-        // println();
         free_fstr_vec(pieces);
-
     }
-    printf("out_has_vertex: %s\n", bool2str(out_has_vertex));
-
-    v[v_count].name[0] = 'o';
-    v[v_count].name[1] = 'u';
-    v[v_count].name[2] = 't';
-    v[v_count].name[3] = '\0';
-    v[v_count].id = pack_str("out");
-    v[v_count].n_edges = 0;
-    v[v_count].edges = NULL;
-    v_count++;
-
-
-
-    for (size_t i = 0; i < v_count; i++) {
-        printf("v[%zu]: ", i);
-        print_vertex(&v[i]);
-    }
-    //
-    // size_t out_id = pack_str("out");
-    // printf("out_id: %zu\n", out_id);
-    // size_t out_idx = id_to_index[out_id];
-    // printf("out_idx: %zu\n", out_idx);
-
+    v[v_count - 1].id = pack_str("out");
+    v[v_count - 1].n_edges = 0;
+    v[v_count - 1].edges = NULL;
+    strcpy(v[v_count - 1].name, "out");
+    id_to_index[v[v_count - 1].id] = v_count - 1;
+    // for (size_t i = 0; i < v_count; i++) {
+    //     print_vertex(&v[i]);
+    // }
+    printf("v_count: %zu\n", v_count);
 
     graph = v;
-    size_t path_count1 = 0;
 
 
-    println();
-    size_t srv_id = pack_str("srv");
-    printf("srv_id: %zu\n", srv_id);
-    size_t out_id = pack_str("out");
-    printf("out_id: %zu\n", out_id);
-    size_t dac_id = pack_str("dac");
-    printf("dac_id: %zu\n", dac_id);
-    size_t fft_id = pack_str("fft");
-    printf("fft_id: %zu\n", fft_id);
     size_t path_count = 0;
 
+    for (size_t i = 0; i < MAX_NODE; i++) {
+        for (size_t j = 0; j < MAX_NODE; j++) {
+            cache[i][j] = -1;
+        }
+    }
 
 
-
-    //
-    // size_t s_d = count_paths(srv_id, dac_id);printf("srv => dac: %zu\n", s_d);
-    // size_t s_f = count_paths(srv_id, fft_id);printf("srv => fft: %zu\n", s_f);
-    // size_t d_o = count_paths(dac_id, out_id);  printf("dac => out: %zu\n", d_o);
-    // size_t f_o = count_paths(fft_id, out_id);printf("fft => out: %zu\n", f_o);
-    // size_t d_f = count_paths(dac_id, fft_id);printf("dac => fft: %zu\n", d_f);
-    // size_t f_d = count_paths(fft_id, dac_id);printf("fft => dac: %zu\n", f_d);
-    println();
+    count_paths("svr", "out");
+    count_paths_b("svr", "out", "dac", "fft");
 
 
-    // for (size_t v_i=0; v_i < v_count; v_i++) {
-    //     free_vertex(&v[v_i]);
-    // }
+    // count_paths("svr", "dac");
+    // count_paths("dac", "fft");
+    // count_paths("fft", "dac");
+    // count_paths("fft", "out");
+    // count_paths("dac", "out");
+
+
+    for (size_t v_i = 0; v_i < v_count; v_i++) {
+        free_vertex(&v[v_i]);
+    }
 
     char* answer = malloc(ANSWER_BUFFER_SIZE);
     sprintf(answer, "%zu", path_count);
